8ff83dbaebe74c1eb75bdf186de7ad145d80312d
Clayton <cscavone96@gmail.com>
2017-04-25 17:27:36 -0400

    Individual prefabs with color and strength.

---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour {
+
+    public Color paint;
+    public int str;
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Renderer>().material.color = paint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
