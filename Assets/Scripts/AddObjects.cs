362b1ff6280c99c0f3276c9bb07bbef1019ca381
Satch Puri <sp1796@g.rit.edu>
2017-04-30 21:30:48 -0400

    Adding agents

---
+++ Assets/Scripts/AddObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObjects : MonoBehaviour {
+
+    [SerializeField] GameObject yellow;
+    [SerializeField]
+    GameObject white;
+    [SerializeField]
+    GameObject blue;
+    [SerializeField]
+    GameObject black;
+    [SerializeField]
+    GameObject camera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp("g"))
+        {
+            Instantiate(yellow, camera.transform.position, transform.rotation) ;
+        }
+        if (Input.GetKeyUp("h"))
+        {
+            Instantiate(white, camera.transform.position, transform.rotation);
+        }
+        if (Input.GetKeyUp("j"))
+        {
+            Instantiate(blue, camera.transform.position, transform.rotation);
+        }
+        if (Input.GetKeyUp("k"))
+        {
+            Instantiate(black, camera.transform.position, transform.rotation);
+        }
+    }
+}
